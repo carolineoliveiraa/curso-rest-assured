da92f886ea9d9ee5d3a27d2f7620bc7a5365a138
Jéssica Caroline <jessicacaroline3213@hotmail.com>
2024-06-17 00:58:44 -0300

    test one

--- src/test/java/com/restassured/curso/BaseTest.java
+++ src/test/java/com/restassured/curso/BaseTest.java
@@ -2,6 +2,8 @@ package com.restassured.curso;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.*;
+
 
 public class BaseTest {
 
@@ -13,8 +15,71 @@ public class BaseTest {
                 when().
                     get("https://reqres.in/api/users?page=2").
                 then().log().all().
-                    statusCode(200);
+                    statusCode(200).
+                    body("page",is(2)).
+                    body("per_page", is(6)).
+                    body("total", is(12)).
+                    body("total_pages", is(2)).
+                    body("data.id[0]", is(7)).
+                    body("data.first_name[0]", is("Michael")).
+                    body("data.last_name[0]", is("Lawson")).
+                    body("data.avatar[0]", is("https://reqres.in/img/faces/7-image.jpg"));
 
+    }
+
+    @Test
+    public void testGetListUsers2(){
+
+        RestAssured.given().log().all().
+                queryParam("page", "2").
+                when().
+                get("https://reqres.in/api/users?page=2").
+                then().log().all().
+                statusCode(200).
+                body("page",is(2)).
+                body("per_page", is(6)).
+                body("total", is(12)).
+                body("total_pages", is(2)).
+                body("data.id[0]", is(7)).
+                body("data.first_name[0]", is("Michael")).
+                body("data.last_name[0]", is("Lawson")).
+                body("data.avatar[0]", is("https://reqres.in/img/faces/7-image.jpg"));
+
+    }
+
+    @Test
+    public void testGetSingleUser(){
+
+        RestAssured.given().log().all().
+                when().
+                get("https://reqres.in/api/users/2").
+                then().log().all().
+                statusCode(200).
+                body("data.id", is(2)).
+                body("data.email", is("janet.weaver@reqres.in")).
+                body("data.first_name", is("Janet")).
+                body("data.last_name", is("Weaver")).
+                body("data.avatar", is("https://reqres.in/img/faces/2-image.jpg")).
+                body("support.url", is("https://reqres.in/#support-heading")).
+                body("support.text", is("To keep ReqRes free, contributions towards server costs are appreciated!"));
+
+    }
+
+    @Test
+    public void testGetSingleUserNotFound(){
+
+        RestAssured.given().log().all().
+                when().
+                get("https://reqres.in/api/users/2").
+                then().log().all().
+                statusCode(200).
+                body("data.id", is(2)).
+                body("data.email", is("janet.weaver@reqres.in")).
+                body("data.first_name", is("Janet")).
+                body("data.last_name", is("Weaver")).
+                body("data.avatar", is("https://reqres.in/img/faces/2-image.jpg")).
+                body("support.url", is("https://reqres.in/#support-heading")).
+                body("support.text", is("To keep ReqRes free, contributions towards server costs are appreciated!"));
 
     }
 }
